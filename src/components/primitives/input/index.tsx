3127f973e5db9286d1840f2cf7f325b3511b8be9
verystack <59302430+verystack@users.noreply.github.com>
2022-03-30 22:51:33 +1100

    JSDoc for Input field

--- src/components/primitives/input/index.tsx
+++ src/components/primitives/input/index.tsx
@@ -2,14 +2,52 @@ import React from 'react'
 import {useFormProvider} from "../form-provider";
 import FormAlert from "../form-alert";
 
+/**
+ * An `<input />` component that is accessible. If used with `<FormProvider />`,
+ * it'll also automatically validate the input based on type, and show clear error messages
+ * while linking its value to the FormProvider's onSubmit event.
+ * Regexes, min/max length, and messages are built in, but can be overwritten using optional extra props.
+ *
+ * @alias InputProps
+ */
+
 export interface InputProps extends CompiledJSXPropsOmitRef<HTMLInputElement> {
+	/**
+	 * Name of the input - will be used for the form validation if using FormContext so make sure it's unique.
+	 */
 	name: string,
-	validationRegex?: RegExp,
-	minLength?: number,
-	maxLength?: number,
-	validationErrorMessage?: string
-	required?: boolean,
+	/**
+	 * The type of the field. Notice that "submit" is not included -
+	 * This is an intentional omission, you should be using <button type="submit" />
+	 * instead as there is no need to add a validation layer to a submit field itself.
+	 * */
 	type?: "email" | "text" | "tel" | "number" | "password"
+	/**
+	 * Optional validation regex. Requires the form to be in a <FormProvider /> to work.
+	 * This should only be used if you need custom validation - Generic regex validation for fields based on type
+	 * (i.e. email validation for email fields, password strength validation for password fields)
+	 * is already built in.
+	 * */
+	validationRegex?: RegExp,
+	/**
+	 * Define a minimum length for the field.
+	 * */
+	minLength?: number,
+	/**
+	 * Define a maximum length for the field.
+	 * */
+	maxLength?: number,
+	/**
+	 * Optionally add a custom validation error message. Error messages are already built in, and are done based
+	 * on the type of component and type of error. However, you may add your own custom one. You can use a basic string
+	 * value here, or pass in a function (which will receive an input argument that includes the type of error -
+	 * i.e. a Regex validation error and your function will receive 'pattern' as the argument.
+	 * */
+	validationErrorMessage?: string | ((type: "pattern" | "minLength" | "maxLength" | "required") => string)
+	/**
+	 * Make the field required or not. Defaults to false for all field types.
+	 */
+	required?: boolean,
 }
 
 
@@ -65,7 +103,12 @@ const Input = ({ type = "text", name, validationRegex, minLength, maxLength, val
 				{...registerFn}
 				{...props}
 			/>
-			{formContext && formContext.errors && formContext.errors[name] && (<>
+			{formContext && formContext.errors && formContext.errors[name] && validationErrorMessage &&
+				<FormAlert>
+					{typeof validationErrorMessage === "function" ? validationErrorMessage(formContext.errors[name].type) : validationErrorMessage}
+				</FormAlert>
+			}
+			{formContext && formContext.errors && formContext.errors[name] && !validationErrorMessage && (<>
 				{formContext.errors[name].type === "required" && (
 					<FormAlert>Required</FormAlert>
 				)}
@@ -73,8 +116,9 @@ const Input = ({ type = "text", name, validationRegex, minLength, maxLength, val
 					<>
 						{type === "password" && <FormAlert>This password is not strong enough. Please</FormAlert>}
 						{type === "email" && <FormAlert>This is not a valid email.</FormAlert>}
-						{type === "tel" && <FormAlert>This is not a valid number.</FormAlert>}
-						{type !== "password" && type !== "tel" && type !== "email" && <FormAlert>This is invalid.</FormAlert>}
+						{type === "tel" && <FormAlert>This is not a valid phone number.</FormAlert>}
+						{type === "number" && <FormAlert>This is not a valid phone number.</FormAlert>}
+						{type !== "password" && type !== "tel" && type !== "email" && type !== "number" && <FormAlert>This is invalid.</FormAlert>}
 					</>
 				)}
 				{formContext.errors[name].type === "maxLength" && (
