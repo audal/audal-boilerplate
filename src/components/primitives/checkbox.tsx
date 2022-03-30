3a37d2f58905a63d128fc9fc6027fd49c495690c
Fafemi Adeola <adeola5678@gmail.com>
2022-03-30 14:36:49 +0100

    primitives/checkbox

--- src/components/primitives/checkbox.tsx
+++ src/components/primitives/checkbox.tsx
@@ -1,3 +1,84 @@
-import { DividerHorizontalIcon, CheckIcon } from '@radix-ui/react-icons';
-import * as Checkbox from '@radix-ui/react-checkbox';
+/** @jsxImportSource @compiled/react */
+import React from "react";
+// import { violet, blackA } from "@radix-ui/colors";
+import * as RadioGroupPrimitive from "@radix-ui/react-radio-group";
 
+export const RadioGroup = RadioGroupPrimitive.Root;
+interface CheckBoxProps {
+	id: string;
+	children: any;
+	size?: "xs" | "sm" | "md" | "lg" | "xl";
+}
+
+export const CheckBox = ({
+	children,
+	id,
+	size = "xl",
+	...props
+}: CheckBoxProps): JSX.Element => {
+	const foundSize = {
+		xs: "0.75rem",
+		sm: "1rem",
+		md: "1.5rem",
+		lg: "2rem",
+		xl: "3rem",
+	}[size];
+	const innerSize = {
+		xs: "0.35rem",
+		sm: "0.5rem",
+		md: "0.75rem",
+		lg: "1rem",
+		xl: "1.5rem",
+	}[size];
+	console.log(foundSize);
+	return (
+		<div css={{ display: "flex", margin: "10px 0", alignItems: "center" }}>
+			<RadioGroupPrimitive.Item
+				css={{
+					all: "unset",
+					backgroundColor: "white",
+					width: `${foundSize} !important`,
+					height: `${foundSize} !important`,
+					borderRadius: "100%",
+					boxShadow: `0 2px 10px black`,
+					"&:hover": { backgroundColor: "red" },
+					"&:focus": { boxShadow: `0 0 0 2px black` },
+				}}
+				value="default"
+				id={id}
+				{...props}
+			>
+				<RadioGroupPrimitive.Indicator
+					css={{
+						display: "flex",
+						alignItems: "center",
+						justifyContent: "center",
+						width: "100%",
+						height: "100%",
+						position: "relative",
+						"&::after": {
+							content: '""',
+							display: "block",
+							width: `${innerSize} !important`,
+							height: `${innerSize} !important`,
+							borderRadius: "50%",
+							backgroundColor: "red",
+						},
+					}}
+				/>
+			</RadioGroupPrimitive.Item>
+			<label
+				css={{
+					color: "black",
+					fontSize: 15,
+					lineHeight: 1,
+					userSelect: "none",
+					paddingLeft: 15,
+				}}
+				htmlFor={id}
+			>
+				{children}
+			</label>
+		</div>
+	);
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,46 +1,59 @@
 /** @jsxImportSource @compiled/react */
 import React from "react";
 import Logo from "../images/icon.png";
-import {AnimatedImageClipped} from "../components/animations/animated-image-clipped";
+import { AnimatedImageClipped } from "../components/animations/animated-image-clipped";
 import {
 	Accordion,
 	AccordionButton,
 	AccordionIcon,
 	AccordionItem,
-	AccordionPanel
+	AccordionPanel,
 } from "../components/primitives/accordion";
+import { RadioGroup, CheckBox } from "../components/primitives/checkbox";
 import Tooltip from "../components/primitives/tooltip";
 import Spinner from "../components/primitives/spinner";
-import {Modal, ModalCloseButton, ModalContent, ModalTrigger} from "../components/primitives/modal";
+import {
+	Modal,
+	ModalCloseButton,
+	ModalContent,
+	ModalTrigger,
+} from "../components/primitives/modal";
 
 const Index = (): React.ReactElement => (
-  <div css={{width: "100%"}}>
-    <div>Blank Page</div>
-	  <Spinner/>
-	  <Modal>
-		  <ModalTrigger>
-			  Edit profile
-		  </ModalTrigger>
-		  <ModalContent css={{
-		  	backgroundColor: "blue"
-		  }}>
-			  hello
-			  <ModalCloseButton/>
-		  </ModalContent>
-	  </Modal>
-	  <Accordion>
-		  <AccordionItem>
-			  <AccordionButton>
-				  hi
-				  <AccordionIcon />
-			  </AccordionButton>
-			  <AccordionPanel>
-
-				  <Tooltip title="he">hellow</Tooltip>
-			  </AccordionPanel>
-		  </AccordionItem>
-	  </Accordion>
-  </div>
+	<div css={{ width: "100%" }}>
+		<div>Blank Page</div>
+		<Spinner />
+		<Modal>
+			<ModalTrigger>Edit profile</ModalTrigger>
+			<ModalContent
+				css={{
+					backgroundColor: "blue",
+				}}
+			>
+				hello
+				<ModalCloseButton />
+			</ModalContent>
+		</Modal>
+		<Accordion>
+			<AccordionItem>
+				<AccordionButton>
+					hi
+					<AccordionIcon />
+				</AccordionButton>
+				<AccordionPanel>
+					<Tooltip title="he">hellow</Tooltip>
+				</AccordionPanel>
+			</AccordionItem>
+		</Accordion>
+		<RadioGroup defaultValue="default" aria-label="View density">
+			<CheckBox id="r1" value="hello">
+				Hello
+			</CheckBox>
+			<CheckBox id="r2" value="hello2">
+				Hello2
+			</CheckBox>
+		</RadioGroup>
+	</div>
 );
 
 export default Index;
