4077702607d24c3d9e765346343554fe966adef0
verystack <59302430+verystack@users.noreply.github.com>
2022-03-30 23:27:17 +1100

    Update index.tsx

--- src/components/primitives/form-provider/index.tsx
+++ src/components/primitives/form-provider/index.tsx
@@ -17,12 +17,26 @@ export interface IFormContext {
 }
 
 export interface FormProviderProps {
+	/**
+	 * On submit, values from the child form components will be
+	 * returned in the format:
+	 * [key: string]: value (any) - instead of the native
+	 * event.
+	 * This makes it easy to further process the data or send it to an endpoint,
+	 * without worrying about handling.
+	 * */
 	onSubmit: SubmitHandler<FieldValues>
 	children: React.ReactNode | React.ReactNode[] | string | null | undefined
 }
 
 const FormContext = React.createContext<IFormContext>(null as any)
 
+/**
+ * This creates an instance of <form /> that you can use Primitive Input, Select, Checkbox, Radio, etc elements in.
+ * It wraps form elements in a React Context and a form tag so that you can access values and error messages anywhere
+ * within your form. React-Hook-Form is used underneath.
+ * @alias FormProviderProps
+ * */
 const FormProvider = React.forwardRef<HTMLFormElement, FormProviderProps>(({children, onSubmit}, ref) => {
 
 	const { register, handleSubmit, formState: { errors }, setValue, getValues } = useForm();
@@ -45,6 +59,12 @@ const FormProvider = React.forwardRef<HTMLFormElement, FormProviderProps>(({chil
 
 export default FormProvider
 
-export const useFormProvider = (): IFormContext => {
+
+/**
+ * This will give you access to a FormProvider parent's values
+ * if it exists. It is not guaranteed to exist,
+ * so check if it returns values before using.
+ * */
+export const useFormProvider = (): IFormContext | undefined => {
 	return React.useContext(FormContext)
 }
