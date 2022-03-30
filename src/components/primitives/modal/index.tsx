d9a6e68580a7ed3f9d27228960ab423ef7523a60
verystack <59302430+verystack@users.noreply.github.com>
2022-03-30 21:12:42 +1100

    Finish modal

--- src/components/primitives/modal/index.tsx
+++ src/components/primitives/modal/index.tsx
@@ -10,10 +10,21 @@ const overlayShow = keyframes({
 	'100%': { opacity: 1 },
 });
 
+const overlayHide = keyframes({
+	'0%': { opacity: 1 },
+	'100%': { opacity: 0 },
+});
+
 const contentShow = keyframes({
-	'0%': { opacity: 0, transform: 'translate(-50%, -40%)' },
-	'20%': { opacity: 0, transform: 'translate(-50%, -40%)' },
-	'100%': { opacity: 1, transform: 'translate(-50%, -50%)' },
+	'0%': { opacity: 0, transform: 'translateY(10%)' },
+	'20%': { opacity: 0, transform: 'translateY(10%)' },
+	'100%': { opacity: 1, transform: 'translateY(0%)' },
+});
+
+const contentHide = keyframes({
+	'0%': { opacity: 0, transform: 'translateY(0%)' },
+	'80%': { opacity: 0, transform: 'translateY(10%)' },
+	'100%': { opacity: 0, transform: 'translateY(10%)' },
 });
 
 export const Modal = DialogPrimitive.Root;
@@ -22,36 +33,51 @@ export const ModalTrigger = DialogPrimitive.Trigger;
 export const ModalContent = (props: CompiledJSXCustomProps<DialogContentImplProps>) => {
 
 	return (
-		<DialogPrimitive.Portal>
+		<DialogPrimitive.Portal forceMount>
 			<DialogPrimitive.Overlay css={{
 				backgroundColor: "#0009",
 				position: 'fixed',
 				zIndex: 20,
 				inset: 0,
-				animation: `${overlayShow} 250ms forwards`,
+				overflowY: 'auto',
+				display: 'grid',
+				top: 0,
+				left: 0,
+				right: 0,
+				bottom: 0,
+				justifyContent: 'center',
+				alignItems: 'center',
+				"&[data-state='open']": {
+					animation: `${overlayShow} 250ms forwards`,
+				},
+				"&[data-state='closed']": {
+					animation: `${overlayHide} 350ms forwards`,
+				},
 				'@media (prefers-reduced-motion: reduce)': {
 					animation: 'none!important',
 				},
-			}} />
+			}}>
 			<DialogPrimitive.Content css={{
 				backgroundColor: 'white',
 				borderRadius: 6,
 				zIndex: 20,
 				boxShadow: 'hsl(206 22% 7% / 35%) 0px 10px 38px -10px, hsl(206 22% 7% / 20%) 0px 10px 20px -15px',
-				position: 'fixed',
-				top: '50%',
-				left: '50%',
-				transform: 'translate(-50%, -50%)',
 				width: '90vw',
 				maxWidth: '450px',
 				maxHeight: '85vh',
 				padding: 25,
-				animation: `${contentShow} 360ms forwards`,
+				"&[data-state='open']": {
+					animation: `${contentShow} 360ms forwards`,
+				},
+				"&[data-state='closed']": {
+					animation: `${contentHide} 250ms forwards`,
+				},
 				'@media (prefers-reduced-motion: reduce)': {
 					animation: 'none!important'
 				},
 				'&:focus': { outline: 'none' },
 			}} {...props} />
+			</DialogPrimitive.Overlay>
 		</DialogPrimitive.Portal>
 	);
 };
