7aea99c35bd17b58288345dbdfec905836cd29eb
verystack <59302430+verystack@users.noreply.github.com>
2022-03-30 22:52:47 +1100

    Visually Hidden JSDoc

--- src/components/primitives/visually-hidden/index.tsx
+++ src/components/primitives/visually-hidden/index.tsx
@@ -1,19 +1,24 @@
 /** @jsxImportSource @compiled/react */
 import React from 'react';
 
+/**
+ * A component to render text for screen-readers. A simple span with css rules to hide the text in an accessible way.
+ *
+ * @alias VisuallyHiddenProps
+ * */
 export type VisuallyHiddenProps = React.HTMLProps<HTMLDivElement>
 
 const VisuallyHidden = (props: VisuallyHiddenProps): JSX.Element => {
 	return <span css={{
-		border:"0",
-		clip:"rect(0px, 0px, 0px, 0px)",
-		height:"1px",
-		width:"1px",
-		margin:"-1px",
-		padding:"0",
-		overflow:"hidden",
-		whiteSpace:"nowrap",
-		position:"absolute",
+		border: 0,
+		clip: "rect(0px, 0px, 0px, 0px)",
+		height: "1px",
+		width: "1px",
+		margin: "-1px",
+		padding: 0,
+		overflow: "hidden",
+		whiteSpace: "nowrap",
+		position: "absolute",
 	}} {...props} />
 }
 
