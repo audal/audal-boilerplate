6b9ad4fabee486d9af57b7fe920e6504704efae6
Faith Oyebode <47439363+faithoyebode@users.noreply.github.com>
2022-03-31 11:31:35 +0100

    ft(drawer-component)
    Updates

--- src/components/primitives/drawer/index.tsx
+++ src/components/primitives/drawer/index.tsx
@@ -1,9 +1,17 @@
+/** @jsxImportSource @compiled/react */
 import React from 'react';
-import { styled, keyframes, VariantProps, CSS } from '../stitches.config';
+import { keyframes } from '@compiled/react';
+// import { styled, keyframes, VariantProps, CSS } from '../stitches.config';
 import * as DialogPrimitive from '@radix-ui/react-dialog';
+import { ModalCloseButton } from '../modal';
 import { Cross1Icon } from '@radix-ui/react-icons';
 import { overlayStyles } from './Overlay';
 import { IconButton } from './IconButton';
+
+const overlayShow = keyframes({
+	'0%': { opacity: 0 },
+	'100%': { opacity: 1 },
+});
 
 const fadeIn = keyframes({
 	from: { opacity: '0' },
@@ -15,32 +23,33 @@ const fadeOut = keyframes({
 	to: { opacity: '0' },
 });
 
-const StyledOverlay = styled(DialogPrimitive.Overlay, overlayStyles, {
-	position: 'fixed',
-	top: 0,
-	right: 0,
-	bottom: 0,
-	left: 0,
-
-	'&[data-state="open"]': {
-		animation: `${fadeIn} 150ms cubic-bezier(0.22, 1, 0.36, 1)`,
-	},
-
-	'&[data-state="closed"]': {
-		animation: `${fadeOut} 150ms cubic-bezier(0.22, 1, 0.36, 1)`,
-	},
-});
-
 type SheetProps = React.ComponentProps<typeof DialogPrimitive.Root>;
 
-export function Sheet({ children, ...props }: SheetProps) {
-	return (
-		<DialogPrimitive.Root {...props}>
-			<StyledOverlay />
-			{children}
-		</DialogPrimitive.Root>
-	);
-}
+export const DrawerOverlay = () => (
+	<DialogPrimitive.Overlay css={{
+		position: 'fixed',
+		top: 0,
+		right: 0,
+		bottom: 0,
+		left: 0,
+		width: "100%",
+		height: "100%",
+		background: "rgba(0, 0, 0, 0.48)",
+		zIndex: 2000,
+		backdropFilter: "blur(8px)",
+		animation: `${overlayShow} 150ms forwards`,
+		'@media (prefers-reduced-motion: reduce)': {
+			animation: 'none!important',
+		},
+		// '&[data-state="open"]': {
+		// 	animation: `${fadeIn} 150ms cubic-bezier(0.22, 1, 0.36, 1)`,
+		// },
+
+		// '&[data-state="closed"]': {
+		// 	animation: `${fadeOut} 150ms cubic-bezier(0.22, 1, 0.36, 1)`,
+		// },
+	}} />
+)
 
 const slideIn = keyframes({
 	from: { transform: '$$transformValue' },
@@ -52,86 +61,91 @@ const slideOut = keyframes({
 	to: { transform: '$$transformValue' },
 });
 
-const StyledContent = styled(DialogPrimitive.Content, {
-	backgroundColor: '$panel',
-	boxShadow: '$colors$shadowLight 0 0 38px -10px, $colors$shadowDark 0 0 35px -15px',
-	position: 'fixed',
-	top: 0,
-	bottom: 0,
-	width: 250,
+export const DrawerContent = ({children, placement='left', ...props}) => {
 
-	// Among other things, prevents text alignment inconsistencies when dialog can't be centered in the viewport evenly.
-	// Affects animated and non-animated dialogs alike.
-	willChange: 'transform',
-
-	// '&:focus': {
-	//   outline: 'none',
-	// },
-
-	'&[data-state="open"]': {
-		animation: `${slideIn} 150ms cubic-bezier(0.22, 1, 0.36, 1)`,
-	},
-
-	'&[data-state="closed"]': {
-		animation: `${slideOut} 150ms cubic-bezier(0.22, 1, 0.36, 1)`,
-	},
-
-	variants: {
-		side: {
-			top: {
-				$$transformValue: 'translate3d(0,-100%,0)',
-				width: '100%',
-				height: 300,
-				bottom: 'auto',
-			},
-			right: {
-				$$transformValue: 'translate3d(100%,0,0)',
-				right: 0,
-			},
-			bottom: {
-				$$transformValue: 'translate3d(0,100%,0)',
-				width: '100%',
-				height: 300,
-				bottom: 0,
-				top: 'auto',
-			},
-			left: {
-				$$transformValue: 'translate3d(-100%,0,0)',
-				left: 0,
-			},
+	const placementStyles = {
+		top: {
+			transform: 'translate3d(0,-100%,0)',
+			width: '100%',
+			height: 300,
+			bottom: 'auto',
 		},
-	},
+		right: {
+			transform: 'translate3d(100%,0,0)',
+			right: 0,
+		},
+		bottom: {
+			transform: 'translate3d(0,100%,0)',
+			width: '100%',
+			height: 300,
+			bottom: 0,
+			top: 'auto',
+		},
+		left: {
+			transform: 'translate3d(0%,0,0)',
+			left: 0,
+		},
+	}[placement];
+	
+	return (
+		<DialogPrimitive.Content 
+		className="drawer-content-wrapper"
+		style={placementStyles}			
+		css={{
+			backgroundColor: 'white',
+			boxShadow: "0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)",
+			position: 'fixed',
+			top: 0,
+			bottom: 0,
+			width: "320px",
+			zIndex: 2000,
+			transition: "transform 1s",
+			// Among other things, prevents text alignment inconsistencies when dialog can't be centered in the viewport evenly.
+			// Affects animated and non-animated dialogs alike.
+			// willChange: 'transform',
 
-	defaultVariants: {
-		side: 'right',
-	},
-});
+			// '&[data-state="open"]': {
+			// 	animation: `${slideIn} 150ms cubic-bezier(0.22, 1, 0.36, 1)`,
+			// },
 
-const StyledCloseButton = styled(DialogPrimitive.Close, {
-	position: 'absolute',
-	top: '$2',
-	right: '$2',
-});
+			// '&[data-state="closed"]': {
+			// 	animation: `${slideOut} 150ms cubic-bezier(0.22, 1, 0.36, 1)`,
+			// },
 
-type SheetContentVariants = VariantProps<typeof StyledContent>;
+			// ...props
+			}} 
+		>
+			{children}
+		</DialogPrimitive.Content>
+	);
+};
+
+// export const CustomTriggerButton = () => {
+// 	retur
+// }
+
+// type SheetContentVariants = VariantProps<typeof StyledContent>;
 type DialogContentPrimitiveProps = React.ComponentProps<typeof DialogPrimitive.Content>;
-type SheetContentProps = DialogContentPrimitiveProps & SheetContentVariants & { css?: CSS };
+// type SheetContentProps = DialogContentPrimitiveProps & SheetContentVariants & { css?: CSS };
 
-export const SheetContent = React.forwardRef<
-	React.ElementRef<typeof StyledContent>,
-	SheetContentProps
-	>(({ children, ...props }, forwardedRef) => (
-	<StyledContent {...props} ref={forwardedRef}>
-		{children}
-		<StyledCloseButton asChild>
-			<IconButton variant="ghost">
-				<Cross1Icon />
-			</IconButton>
-		</StyledCloseButton>
-	</StyledContent>
-));
+export const DrawerComponent = ({ children, contentPlacement, ...props }) => (
+	<DialogPrimitive.Root>
+		<DialogPrimitive.Trigger>
+			<div>Edit Profile</div>
+		</DialogPrimitive.Trigger>
+		<DialogPrimitive.Portal>
+			<DrawerOverlay />
+			<DrawerContent placement={contentPlacement} {...props}>
+				<ModalCloseButton />
+				{children}
+			</DrawerContent>
+		</DialogPrimitive.Portal>
+	</DialogPrimitive.Root>
+	
+);
 
 export const SheetTrigger = DialogPrimitive.Trigger;
-export const SheetClose = DialogPrimitive.Close;
+// export DrawerOverlay = DrawerOverlay;
+export const DrawerCloseButton = ModalCloseButton;
 export const SheetTitle = DialogPrimitive.Title;
 export const SheetDescription = DialogPrimitive.Description;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,9 +12,21 @@ import {
 import Tooltip from "../components/primitives/tooltip";
 import Spinner from "../components/primitives/spinner";
 import {Modal, ModalCloseButton, ModalContent, ModalTrigger} from "../components/primitives/modal";
+import { DrawerComponent } from "../components/primitives/drawer";
 
 const Index = (): React.ReactElement => (
   <div css={{width: "100%"}}>
+	<DrawerComponent>
+		<div>
+			Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+			Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+			when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+			It has survived not only five centuries, but also the leap into electronic typesetting, 
+			remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset 
+			sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like 
+			Aldus PageMaker including versions of Lorem Ipsum.
+		</div>
+	</DrawerComponent>
     <div>Blank Page</div>
 	  <Spinner/>
 	  <Modal>
@@ -40,6 +52,7 @@ const Index = (): React.ReactElement => (
 			  </AccordionPanel>
 		  </AccordionItem>
 	  </Accordion>
+	  
   </div>
 );
 
