90ead502c009d7cc9bfe2841d9da575a6cdd2197
verystack <59302430+verystack@users.noreply.github.com>
2022-04-05 02:04:28 +1000

    Update index.tsx

--- src/components/primitives/button/index.tsx
+++ src/components/primitives/button/index.tsx
@@ -4,7 +4,7 @@ import {ClassNames} from "@compiled/react";
 import { ax } from '@compiled/react/runtime';
 import Spinner from "../spinner";
 
-interface ButtonProps extends CompiledJSXProps<HTMLButtonElement> {
+export interface ButtonProps extends CompiledJSXProps<HTMLButtonElement> {
 	loading?: boolean
 	type: "button" | "submit" | "reset" | undefined
 }
@@ -64,71 +64,6 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(({ disabled, loa
 							{children}
 						</span>
 		</button>
-	)
-	return (
-		<div className={className} css={{cursor: 'pointer', background: 'blue', height: '100px', width: '100px'}} />
-	)
-
-	return (
-		<ClassNames>
-			{({ css, style }) => {
-				return (
-					//@ts-ignore
-					<button
-						type={type}
-						role="button"
-						aria-disabled={disabled}
-						disabled={disabled || loading}
-						/**
-						* Can buttons that have a loading state be considered a toggle button?
-						* Not sure...
-						*/
-						aria-pressed={typeof loading !== undefined ? loading : undefined}
-						className={ax([css({
-							cursor: "pointer",
-							appearance: "none",
-							display: "inline-flex",
-							justifyContent: "center",
-							alignItems: "center",
-							position: "relative",
-							userSelect: "none",
-							whiteSpace: "nowrap",
-							verticalAlign: "middle",
-							outlineOffset: "2px",
-							width: "auto",
-							transition: 'opacity 0.3s',
-							lineHeight: "1.2",
-							"&[disabled]": {
-								cursor: "not-allowed",
-								opacity: loading ? 0.8 : 0.4
-							},
-						}), className])}
-						{...style}
-						{...props}
-					>
-						<div aria-hidden={!!loading} css={{
-							width: "100%",
-							height: "100%",
-							display: "flex",
-							alignItems: "center",
-							justifyContent: "center",
-							opacity: loading ? 1 : 0,
-							transition: '0.2s',
-							position: 'absolute',
-							left: 0,
-							top: 0
-						}}><Spinner size="sm"/></div>
-						<span aria-hidden={loading} css={{
-							opacity: loading ? 0 : 1,
-							transition: '0.2s'
-						}}>
-							{children}
-						</span>
-					</button>
-				)
-			}}
-		</ClassNames>
-
 	)
 })
 
